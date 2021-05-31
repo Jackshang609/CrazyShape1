d37f6c4844cc8e52246fdb9e0a271fd5375a99d2
K417s <80661104+K417s@users.noreply.github.com>
2021-05-31 13:20:47 +0800

    2021上機考v3_(成益輝)

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -13,15 +13,21 @@ import com.bumptech.glide.module.AppGlideModule
 public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
+
+    var Flag: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         Toast.makeText(baseContext, "作者：成益輝", Toast.LENGTH_LONG).show()
         GlideApp.with(this)
             .load(R.drawable.cover)
             .override(800, 600)
             .into(imgTitle)
+
+        RndShape()
 
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(p0: View?): Boolean {
@@ -30,5 +36,22 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+
+        imgNext.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                RndShape()
+            }
+        })
+
+    }
+
+    fun RndShape() {
+        Flag = (1..4).random()
+        when (Flag) {
+            1 -> imgNext.setImageResource(R.drawable.circle)
+            2 -> imgNext.setImageResource(R.drawable.square)
+            3 -> imgNext.setImageResource(R.drawable.star)
+            4 -> imgNext.setImageResource(R.drawable.triangle)
+        }
     }
 }
